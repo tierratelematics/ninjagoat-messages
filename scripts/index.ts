13389538c2cfb86d375e5320949bacc9a3b23d0c
Michele Melluso <mmelluso@topcon.com>
2017-10-16 12:03:37 +0200

    Fix exports

---
+++ scripts/index.ts
@@ -0,0 +1,8 @@
+export { default as DefaultConfig } from "./DefaultConfig";
+export { default as IMessagesService } from "./interfaces/IMessagesService";
+export { default as MessagesModule } from "./MessagesModule";
+export { default as MessagesService } from "./MessagesService";
+export { default as NinjagoatMessages } from "./NinjagoatMessages";
+export { IMessagesConfig } from "./interfaces/IMessagesConfig";
+export { MessageType } from "./MessageType";
+export { Origin } from "material-ui/Snackbar";
