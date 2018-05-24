557aafb3b5609bb13865c5cf64694bce3d01dc39
Enrico Calzavara <enrico.calzavara@gmail.com>
2018-05-24 18:28:05 +0200

    Add view property to message data interface

--- scripts/interfaces/IMessageData.ts
+++ scripts/interfaces/IMessageData.ts
@@ -1,10 +1,12 @@
 import { SnackBarOrigin } from "@material-ui/core/Snackbar";
 
 import { MessageType } from "../MessageType";
+import * as React from "react";
 
 export interface IMessageData {
     message: string;
     type: MessageType;
     timeout?: number;
     position?: SnackBarOrigin;
+    view?: (props, onClose) => React.ReactElement<any>;
 }
