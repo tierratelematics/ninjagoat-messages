b1f0bf4f3baa1e4190399dbab9e427140f924299
Enrico Calzavara <enrico.calzavara@gmail.com>
2018-05-24 18:27:18 +0200

    Add view property to messages config interface

--- scripts/interfaces/IMessagesConfig.ts
+++ scripts/interfaces/IMessagesConfig.ts
@@ -1,6 +1,8 @@
+import * as React from "react";
 import { SnackBarOrigin } from "@material-ui/core/Snackbar";
 
 export interface IMessagesConfig {
     timeout: number;
     position: SnackBarOrigin;
+    view?: (props, onClose) => React.ReactElement<any>;
 }
